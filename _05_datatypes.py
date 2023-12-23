1d680e76e28ffc1e899c9c62e332ec64549dc080
ayush931 <ayushkumar9315983@gmail.com>
2023-12-23 21:42:08 +0530

    ptyhon

---
+++ _05_datatypes.py
@@ -0,0 +1,63 @@
+# Number (int :- integers (whole number))
+
+num_int = 42
+print(num_int)
+print(type(num_int))
+
+# Float
+
+num_int_error = 3.14
+print(num_int_error)
+print(type(num_int_error))
+
+# Complex Number
+
+num_complex = 2 + 3j
+print(num_complex)
+print(type(num_complex))
+
+# Boolean
+
+bool_true = True
+print(bool_true)
+print(type(bool_true))
+
+bool_false = False
+print(bool_false)
+print(type(bool_false))
+
+# Set (It contain only unique element)
+
+my_set = {1, 2, 3, 4, 5}
+print(my_set)
+print(type(my_set))
+
+# Dictionary (Store data in key value pair)
+
+my_dict = {'name': 'John', 'age': 25, 'City': 'New York'}
+print(my_dict)
+print(type(my_dict))
+
+# String
+
+my_str = "Hello, world!"
+print(my_str)
+print(type(my_str))
+
+# List or Array, It can be modified.
+
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+print(type(my_list))
+
+# Tuple, it cannot be modified
+
+my_tuple = (1, 'two', 3.0)
+print(my_tuple)
+print(type(my_tuple))
+
+# None :- No valaue datatype
+my_none = None
+print(my_none)
+print(type(my_none))
+
