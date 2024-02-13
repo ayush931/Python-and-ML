6111bcfd6fdcf5d423bdaab61db6eac8b0e8f62e
ayush931 <159576275+ayush931@users.noreply.github.com>
2024-02-13 17:53:48 +0530

    Docstring Comment

---
+++ 03_DocstringComments.py
@@ -0,0 +1,13 @@
+# Docstring Comments
+
+def Physics_wallah(PWSkills, IOI):
+    '''
+    This is a docstirng for my_function.
+    
+    :PWSkills: Description of PW Skills
+    :IOI: Description of IOI
+    :return: Description of the return value
+    
+    '''
+    
+    # Function code here
