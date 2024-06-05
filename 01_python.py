9725335596ad2b9b5923e92c59146b7904ee669c
ayush931 <159576275+ayush931@users.noreply.github.com>
2024-06-05 13:01:39 +0530

    Basic python

--- 01_python.py
+++ 01_python.py
@@ -1,3 +1,39 @@
-print ("Hello World")
+print ("hello world")
 
-help ("keywords") # It will give the list of the keyword. It cannot be used anywhere as a naming of variable.
+# Variables are placeholder where we will keep variety of data which we can use to write logic
+
+a = 3 
+print (a)
+
+print(type(a)) # int
+
+b = 4.2 # float
+print(type(b))
+
+c = True-False
+print (c)
+
+# True is 1 and False is 0
+
+d = 5 + 6j
+print(type(d))
+print (d.real)
+
+#! Rules of defining variable
+
+#? 1. A variable start with a letter or underscore character but not with number or special character.
+#? 2. don't use keyword as variable
+
+# Indentation 
+if 3 > 2:
+    print("Greater")
+
+# Standard input-output
+
+name = input("Please enter your name: ")
+print(name)
+
+a = "1"
+print(a)
+print(type(a))
+print (int(a)) # Change string into integer
---
+++ 02_mutability.py
@@ -0,0 +1,16 @@
+a = "Ayush"
+print(a[1])
+
+list_cont = [1, 3, 5, "Ayush", 3 + 4j, True, "a"]
+print(type(list_cont))
+
+list_cont[3] = "Aman"
+
+print (list_cont)
+
+# Objects / container whose state or value can change after they are created are called mutable.
+
+a[2] = "k"
+# Can't change the value of string.
+
+# List and dictionary are mutable but string and tuple are immutable.
